ec9c5ce6507f8ab21268adb86ee8465f5b2dc138
ksolis <limonsito.kls@hotmail.com>
2024-03-01 00:21:51 +0100

    mmicroservices

---
+++ springtoolsuite/src/main/java/com/limoncitoscompany/springtoolsuite/SpringtoolsuiteApplication.java
@@ -0,0 +1,13 @@
+package com.limoncitoscompany.springtoolsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtoolsuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtoolsuiteApplication.class, args);
+	}
+
+}
